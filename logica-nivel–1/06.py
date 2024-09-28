de6c6e801a0af24cf9e0013fb624d7fa4b5f7b8a
Elerrandro <elerrandrosousa@gmail.com>
2024-09-28 12:46:38 -0300

    Create 06.py

---
+++ logica-nivel–1/06.py
@@ -0,0 +1,10 @@
+#6. Menu - Elabore um programa que mostre o seguinte menu na tela:
+
+print("Cadastro de Clientes")
+print("0 - Fim")
+print("1 - Inclui")
+print("2 - Altera")
+print("3 - Exclui")
+print("4 - Consulta")
+print("")
+print("Opção: ")
