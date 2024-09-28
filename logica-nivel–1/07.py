8b2bf19cce9bdedbb9715f8046ea84c053b164a3
Elerrandro <elerrandrosousa@gmail.com>
2024-09-28 12:48:34 -0300

    Create 07.py

---
+++ logica-nivel–1/07.py
@@ -0,0 +1,13 @@
+#7. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.Enriqueça o desenho com outros caracteres, simulando enfeites.
+
+print("       X")
+print("      XXX")
+print("     XXXXX")
+print("    XXXXXXX")
+print("   XXXXXXXXX")
+print("  XXXXXXXXXXX")
+print(" XXXXXXXXXXXXX")
+print("XXXXXXXXXXXXXXX")
+print("       XX")
+print("       XX")
+print("      XXXX")
