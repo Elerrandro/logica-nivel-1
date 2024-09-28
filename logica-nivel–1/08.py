17e52d227bf08f5ca35fe019323a9439c0c1be56
Elerrandro <elerrandrosousa@gmail.com>
2024-09-28 12:55:01 -0300

    Create 08.py

---
+++ logica-nivel–1/08.py
@@ -0,0 +1,4 @@
+#8. Implemente um programa que na primeira linha faça um print perguntando o nome do usuário e, na linha 2, chame a função input() para capturar o valor do teclado e armazenar na memória do computador.
+
+print("Digite seu nome: ")
+input()
