586da6d573875de16fbb950eab1edf2a10f1ba67
Elerrandro <elerrandrosousa@gmail.com>
2024-09-28 12:39:28 -0300

    Create 02.py

---
+++ logica-nivel–1/02.py
@@ -0,0 +1,5 @@
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+print("Nome: Raimundo")
+print("Estado: Rio de janeiro")
+print("Município: Rio de janeiro")
